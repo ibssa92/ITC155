c506d0f035c3dc6181420067ea5352ad7a751e08
ibssa92 <41655642+ibssa92@users.noreply.github.com>
2019-04-16 11:07:10 -0700

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+
+		// create an arraylist of Integers
+		ArrayList<String> list = new ArrayList<>();
+
+		// populate the arraylist
+		list.add("walk");
+		list.add("talk");
+		list.add("speak");
+		list.add("run");
+		
+		removeEvenLength(list);
+		System.out.println(list);
+		
+
+	}
+
+	public static void removeEvenLength(ArrayList<String> array) {
+
+		for (int i = 0; i < array.size(); i++) {
+			String word = array.get(i);
+			if (word.length() % 2 == 0) {
+				array.remove(i);
+				i--;
+			}
+		}
+
+	}
+
+}
